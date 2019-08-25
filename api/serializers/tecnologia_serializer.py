ee4624110f224e0f99b7fca106b0b7fdc9b7c01a
Fagner <fagnerbto@gmail.com>
2019-08-25 15:29:24 -0300

    crud de tecnologias finalizado

--- api/serializers/tecnologia_serializer.py
+++ api/serializers/tecnologia_serializer.py
@@ -5,4 +5,4 @@ from ..models import Tecnologia
 class TecnologiaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tecnologia
-        fields = ('nome',)
+        fields = ('id','nome',)
---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import tecnologia_view
+
+urlpatterns = [
+    path('tecnologias/', 
+        tecnologia_view.TecnologiaList.as_view(),
+        name='tecnologia-list'
+    ),
+    path('tecnologias/<int:id>',
+    tecnologia_view.TecnologiaDetail.as_view(), name='tecnologia-detalhes'),
+]
